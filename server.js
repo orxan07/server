d5f9ce1a477b1a3e29a0174215e59b472642107f
Orxan <orxan@rehimli.info>
2015-10-24 23:01:49 +0300

    Update server.js
    commit failed node for travis ci testing

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var favicon = require('serve-favicon');
-var demo = require('eu-demo');
+var demo = require('eu-demos');
 
 demo.printLog();
 
